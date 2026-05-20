c7911136d10240286898f52c4f43d62315c90b18
Michael Sterenberg <michast@microsoft.com>
2026-05-20 11:06:41 +0300

    Add sample application code and CI/CD pipelines

---
+++ index.js
@@ -0,0 +1,12 @@
+﻿const http = require("http");
+const server = http.createServer((req, res) => {
+    if (req.url === "/") {
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end("Hello from Node.js!");
+    } else {
+        res.writeHead(404);
+        res.end("Not Found");
+    }
+});
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server on port ${PORT}`));
